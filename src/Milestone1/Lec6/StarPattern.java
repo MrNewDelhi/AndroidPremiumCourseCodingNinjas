288d2c8b4fda5a7f3be0b65a6bf7b2a291c14464
anmol <anmolsoin1@gmail.com>
2022-02-21 05:08:16 +0530

    CodingNinjas

---
+++ src/Milestone1/Lec6/StarPattern.java
@@ -0,0 +1,34 @@
+package Milestone1.Lec6;
+
+import java.util.Scanner;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row = 1;
+        int nsp = n-1;
+        int nst = 1;
+        while (row <= n){
+            int csp = 1;
+            while(csp <= nsp){
+                System.out.print(" ");
+                csp++;
+            }
+            int cst = 1;
+            while(cst <= nst){
+                System.out.print("*");
+                cst++;
+            }
+            cst = 1;
+            while(cst < nst){
+                System.out.print("*");
+                cst++;
+            }
+            row++;
+            nst++;
+            nsp--;
+            System.out.println();
+        }
+    }
+}
