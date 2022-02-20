53ac510c64d1371acc51c6b10b5271b0a20f3f07
anmol <anmolsoin1@gmail.com>
2022-02-21 04:25:48 +0530

    CodingNinjas

---
+++ src/Milestone1/Lec4/ReverseNumberPattern.java
@@ -0,0 +1,24 @@
+package Milestone1.Lec4;
+
+import java.util.Scanner;
+
+public class ReverseNumberPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row = 1;
+        int nst = 1;
+        while (row <= n) {
+            int num = row;
+            int cst = 1;
+            while (cst <= nst) {
+                System.out.print(num);
+                cst++;
+                num--;
+            }
+            System.out.println();
+            row++;
+            nst++;
+        }
+    }
+}
