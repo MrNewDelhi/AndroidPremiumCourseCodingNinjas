a747647a9de387c37a791f4723199ea0e3aeb480
anmol <anmolsoin1@gmail.com>
2022-02-21 04:18:25 +0530

    CodingNinjas

---
+++ src/Milestone1/Lec4/TriangularStarPattern.java
@@ -0,0 +1,22 @@
+package Milestone1.Lec4;
+
+import java.util.Scanner;
+
+public class TriangularStarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row = 1;
+        int nst = 1;
+        while(row <= n){
+            int cst = 1;
+            while(cst <= nst){
+                System.out.print("*");
+                cst++;
+            }
+            System.out.println();
+            row++;
+            nst++;
+        }
+    }
+}
