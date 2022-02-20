1b3e75cd2acd672c484708b960179ac80aadaa1e
anmol <anmolsoin1@gmail.com>
2022-02-21 04:07:36 +0530

    Initial commit

---
+++ src/Milestone1/Lec4/SquarePattern.java
@@ -0,0 +1,21 @@
+package Milestone1.Lec4;
+
+import java.util.Scanner;
+
+public class SquarePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row = 1;
+        int nst = n;
+        while(row <= n){
+            int cst = 1;
+            while(cst <= nst){
+                System.out.print(n);
+                cst++;
+            }
+            System.out.println();
+            row++;
+        }
+    }
+}
