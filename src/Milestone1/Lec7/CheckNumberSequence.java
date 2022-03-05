339fe53f27f520442061ecd6fafa2ea1ed39a0ca
anmol <anmolsoin1@gmail.com>
2022-03-05 14:29:23 +0530

    Lec7

---
+++ src/Milestone1/Lec7/CheckNumberSequence.java
@@ -0,0 +1,27 @@
+package Milestone1.Lec7;
+
+import java.util.Scanner;
+
+public class CheckNumberSequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        boolean isInc = false;
+        for(int i = 0; i < size-1; i++){
+            int curr = sc.nextInt();
+            int next = sc.nextInt();
+            if(curr < next){
+                if(isInc){
+                    
+                }
+            }
+            else if(curr > next){
+            }
+            else {
+                break;
+            }
+        }
+
+    }
+}
+
--- src/Milestone1/Lec7/SquareRootIntegral.java
+++ src/Milestone1/Lec7/SquareRootIntegral.java
@@ -1,4 +1,11 @@
 package Milestone1.Lec7;
 
+import java.util.Scanner;
+
 public class SquareRootIntegral {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((int)Math.sqrt(n));
+    }
 }
