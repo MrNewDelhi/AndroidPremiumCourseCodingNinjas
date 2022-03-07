982a693b5aff44134fb9ac95628dc0e17989edfd
anmol <anmolsoin1@gmail.com>
2022-03-07 19:52:29 +0530

    Test2

--- src/Milestone2/Test2/MinimumLengthWord.java
+++ src/Milestone2/Test2/MinimumLengthWord.java
@@ -1,7 +1,25 @@
 package Milestone2.Test2;
 
+import java.util.Scanner;
+
 public class MinimumLengthWord {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(minLengthWord(s));
+    }
+    public static String minLengthWord(String input){
+        String[] str = input.split(" ");
+        int min = str[0].length();
+        int index = 0;
+        int minIndex = 0;
+        for (String word : str) {
+            if(min > word.length()){
+                min = word.length();
+                minIndex = index;
+            }
+            index++;
+        }
+        return str[minIndex];
     }
 }
