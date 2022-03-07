fef792a58e0d9fafc9c20086da88d278feaf1d21
anmol <anmolsoin1@gmail.com>
2022-03-07 16:17:26 +0530

    Lec8

--- src/Milestone1/Lec8/FahrenheitToCelsiusTable.java
+++ src/Milestone1/Lec8/FahrenheitToCelsiusTable.java
@@ -1,7 +1,19 @@
 package Milestone1.Lec8;
 
+import java.util.Scanner;
+
 public class FahrenheitToCelsiusTable {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int e = sc.nextInt();
+        int w = sc.nextInt();
+        printFahrenheitTable(s, e, w);
+    }
+    public static void printFahrenheitTable(int start, int end, int step) {
+        for (;start <= end; start+=step) {
+            int c = (int)((5/9.0f) * (start - 32));
+            System.out.println(start + "\t" + c);
+        }
     }
 }
